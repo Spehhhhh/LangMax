18be7712cb5b37435abe17418cfffb4efddfd131
Alex <30626291+ipruning@users.noreply.github.com>
2023-11-27 11:39:06 +0800

    chore: update shell scripts for LLVM compilation
    - Update the file `23q4/llama.cpp/sta_inference.sh`
    - Update the file `23q4/llama.cpp/sta_server.sh`
    - Update the file `23q4/llama.cpp/upd_inference.sh`

--- 23q4/llama.cpp/sta_inference.sh
+++ 23q4/llama.cpp/sta_inference.sh
@@ -1,7 +1,7 @@
 export MODEL="/Volumes/Workspace/Databases/Models/openhermes-2.5-mistral-7b-16k.Q5_K_M.gguf"
 export PROMPT="I have three apples, I eat two pears, how many apples do I have?"
 
-"$HOME"/Coding/llama.cpp/main \
+"$HOME"/Databases/Stacks/LLM/llama.cpp/main \
     --model "${MODEL}" \
     --prompt "${PROMPT}" \
     --color \
--- 23q4/llama.cpp/sta_server.sh
+++ 23q4/llama.cpp/sta_server.sh
@@ -1,5 +1,5 @@
 export MODEL="/Volumes/Workspace/Databases/Models/openhermes-2.5-mistral-7b-16k.Q5_K_M.gguf"
 export PROMPT="I have three apples, I eat two pears, how many apples do I have?"
 
-"$HOME"/Coding/llama.cpp/server \
+"$HOME"/Databases/Stacks/LLM/llama.cpp/server \
     -m "${MODEL}"
