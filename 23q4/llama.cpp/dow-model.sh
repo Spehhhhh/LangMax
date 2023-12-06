afdc7bca84565a547aa134a8e3a45e091856a54f
Alex <30626291+ipruning@users.noreply.github.com>
2023-12-06 20:34:20 +0800

    chore: update file paths and names
    - Update `SPACE_NAME` to `"TheBloke/deepseek-llm-67b-chat-GGUF"`
    - Update `MODEL_NAME` to `"deepseek-llm-67b-chat.Q5_K_M.gguf"`
    - Update `MODELS_DIR` to `"/Volumes/Workspace/Databases/Models"`

--- 23q4/llama.cpp/dow-model.sh
+++ 23q4/llama.cpp/dow-model.sh
@@ -1,6 +1,6 @@
-export SPACE_NAME="" # TheBloke/Yi-6B-GGUF
-export MODEL_NAME="" # yi-6b.Q5_K_M.gguf
-export MODELS_DIR="" # /Volumes/Workspace/Databases/Models
+export SPACE_NAME="TheBloke/deepseek-llm-67b-chat-GGUF" # TheBloke/Yi-6B-GGUF
+export MODEL_NAME="deepseek-llm-67b-chat.Q5_K_M.gguf"   # yi-6b.Q5_K_M.gguf
+export MODELS_DIR="/Volumes/Workspace/Databases/Models" # /Volumes/Workspace/Databases/Models
 
 huggingface-cli download "${SPACE_NAME}" "${MODEL_NAME}" \
     --local-dir "${MODELS_DIR}" \
