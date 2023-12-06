13055f60f4ad1ecee7ae4a66f931624f1a952a48
Alex <30626291+ipruning@users.noreply.github.com>
2023-12-06 17:19:26 +0800

    feat: add new scripts for sta inference and training
    - Add new file `sta_inference_llama.sh` in `23q4/mlx` directory
    - Add new file `sta_inference_mistral.sh` in `23q4/mlx` directory
    - Add new file `sta_train_lora.sh` in `23q4/mlx` directory

---
+++ 23q4/mlx/sta_train_lora.sh
@@ -0,0 +1,9 @@
+export MLX_PATH="$HOME/Databases/Stacks/LLM/MLX/mlx-examples"
+export MODEL_PATH="/Volumes/Workspace/Databases/Models/Llama-2-7B"
+
+"$MLX_PATH"/.venv/bin/python "$MLX_PATH"/lora/lora.py \
+  --model "$MODEL_PATH"/mlx_llama_weights.npz \
+  --tokenizer "$MODEL_PATH"/tokenizer.model \
+  --train \
+  --iters 600 \
+  --adapter_file "$MODEL_PATH"/adapters.npz
