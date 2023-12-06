0bd7b3880af8ca19580a06209b3d016b1c6aec9a
Alex <30626291+ipruning@users.noreply.github.com>
2023-12-06 17:19:26 +0800

    feat: add new scripts for sta inference and training
    - Add new file `sta_inference_llama.sh` in `23q4/mlx` directory
    - Add new file `sta_inference_mistral.sh` in `23q4/mlx` directory
    - Add new file `sta_train_lora.sh` in `23q4/mlx` directory

---
+++ 23q4/mlx/sta_inference_mistral.sh
@@ -0,0 +1,8 @@
+export MLX_PATH="$HOME/Databases/Stacks/LLM/MLX/mlx-examples"
+export MODEL_PATH="/Volumes/Workspace/Databases/Models/Mistral-7B-v0.1"
+export PROMPT="I have three apples, I eat two pears, how many apples do I have? I think I have"
+
+"$MLX_PATH"/.venv/bin/python "$MLX_PATH"/mistral/mistral.py \
+  --model_path "$MODEL_PATH" \
+  --prompt "$PROMPT" \
+  --temp 0
