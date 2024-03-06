84ca97edc7ddaee4cac2a96cedb9d94ada6171a1
Alex <30626291+ipruning@users.noreply.github.com>
2024-03-06 15:57:49 +0800

    chore: update variable names for consistency across files
    - Update the `SPACE_NAME` and `MODEL_NAME` variables in `llama.cpp/dow-model.sh`
    - Update the `GGUF_MODEL_PATH` and `PROMPT` variables in `llama.cpp/sta-inference.sh`
    - Update the `MODEL_PATH` variable in `mlx/sta-inference-mistral.sh`

--- 23q4/llama.cpp/sta-inference.sh
+++ 23q4/llama.cpp/sta-inference.sh
@@ -1,6 +1,6 @@
 export LLAMACPP_DIR="$HOME"/Databases/Stacks/LLM/llama.cpp
-export GGUF_MODEL_PATH="/Volumes/Workspace/Databases/Models/openhermes-2.5-mistral-7b-16k.Q5_K_M.gguf"
-export PROMPT="I have three apples, I eat two pears, how many apples do I have?"
+export GGUF_MODEL_PATH="/Volumes/Workspace/Databases/Models/mistral-7b-instruct-v0.1.Q5_K_M.gguf"
+export PROMPT="I have three apples, I eat two pears, how many apples do I have? Give me ten examples"
 
 "$LLAMACPP_DIR"/main \
     --model "${GGUF_MODEL_PATH}" \
--- 23q4/mlx/sta-inference-mistral.sh
+++ 23q4/mlx/sta-inference-mistral.sh
@@ -1,5 +1,6 @@
 export MLX_PATH="$HOME/Databases/Stacks/LLM/MLX/mlx-examples"
-export MODEL_PATH="/Volumes/Workspace/Databases/Models/Mistral-7B-v0.1"
+# export MODEL_PATH="/Volumes/Workspace/Databases/Models/Mistral-7B-v0.1"
+export MODEL_PATH="/Users/alex/Databases/Stacks/LLM/MLX/mlx-examples/mistral/Mistral-7B-v0.1"
 export PROMPT="I have three apples, I eat two pears, how many apples do I have? I think I have"
 
 "$MLX_PATH"/.venv/bin/python "$MLX_PATH"/mistral/mistral.py \
