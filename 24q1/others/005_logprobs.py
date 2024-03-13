79d3075c141bf0688bb0bf0e24ebee58dc4a744c
Alex <30626291+ipruning@users.noreply.github.com>
2024-03-13 15:40:59 +0800

    feat: update OpenAI API configuration and introduce new files
    - Update the OpenAI API key in `24q1/others/004_claude_3.py`
    - Add missing imports in `24q1/others/005_logprobs.py`
    - Add a new python file `24q1/others/006_pools.py` with OpenAI API configuration
    - Introduce a new python file `24q1/others/007_claude_3_vision.py` for anthropic messages
    - Add a new file `24q1/others/008_chainlit.py` with a comment only
    - Introduce a new python file `24q1/others/009_chainlit_llava.py` for class definitions and conversation setup
    - Add a new file `24q1/others/010_openlogprobs.py` for log probability extraction using OpenLogProbs
    - Include a new python file `24q1/others/011_ragas_gen_ss_ts.py` for generating test sets from Semantic Scholar data

--- 24q1/others/005_logprobs.py
+++ 24q1/others/005_logprobs.py
@@ -1,10 +1,15 @@
-from openai import OpenAI
-from math import exp
-import numpy as np
-from IPython.display import display, HTML
 import os
+from math import exp
 
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "<your OpenAI API key if not set as env var>"))
+import numpy as np
+from IPython.display import HTML, display
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get(
+        "OPENAI_API_KEY", "<your OpenAI API key if not set as env var>"
+    )
+)
 
 
 def get_completion(
