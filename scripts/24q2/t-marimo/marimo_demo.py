84d70bca3e3f95896413b02c66cfa0b4ab923faf
Alex <30626291+ipruning@users.noreply.github.com>
2024-04-23 14:13:19 +0800

    rename

---
+++ scripts/24q2/t-marimo/marimo_demo.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.4.2"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def __():
+    1+1
+    return
+
+
+@app.cell
+def __():
+    3+3
+    return
+
+
+if __name__ == "__main__":
+    app.run()
